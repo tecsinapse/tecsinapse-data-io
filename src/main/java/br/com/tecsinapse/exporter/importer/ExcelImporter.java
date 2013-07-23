0859e1bf6dc463a926b7b1d583705fdd38cb5208
Filipe Névola <filipe.nevola@tecsinapse.com.br>
2013-07-23 14:22:09 -0300

    [#53515947] Upload dos Dados de Vendas - criando forma genérica de ler CSV e Excel

--- src/main/java/br/com/tecsinapse/exporter/importer/ExcelImporter.java
+++ src/main/java/br/com/tecsinapse/exporter/importer/ExcelImporter.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+
 public class ExcelImporter<T> {
 
     private final Class<T> clazz;
@@ -25,6 +26,7 @@ public class ExcelImporter<T> {
         this(clazz);
         this.excel = file;
     }
+
     public ExcelImporter(Class<T> clazz, InputStream inputStream) throws IOException {
         this(clazz);
         this.excelInputStream = inputStream;
@@ -37,24 +39,23 @@ public class ExcelImporter<T> {
     public <X> List<T> parse() throws IllegalAccessException, InstantiationException, InvocationTargetException, IOException, InvalidFormatException {
         List<T> list = new ArrayList<>();
         Set<Method> methods = ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(TableCellMapping.class));
-        Workbook wb =  getWorkbook();
+        Workbook wb = getWorkbook();
         Sheet sheet = wb.getSheetAt(0);
 
         Iterator<Row> rowIterator = sheet.iterator();
 
         boolean header = true;
-        while(rowIterator.hasNext()) {
+        while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
-            if(header) {
+            if (header) {
                 header = false;
                 continue;
             }
-            List<String> fields = getFields(row);
             T instance = clazz.newInstance();
 
             for (Method method : methods) {
                 TableCellMapping tcm = method.getAnnotation(TableCellMapping.class);
-                String value = getValueOrEmpty(fields, tcm.columnIndex());
+                String value = getValueOrEmpty(row.getCell(tcm.columnIndex()));
                 TableCellConverter converter = tcm.converter().newInstance();
                 Object obj = converter.apply(value);
                 method.invoke(instance, obj);
@@ -65,34 +66,39 @@ public class ExcelImporter<T> {
     }
 
     private List<String> getFields(Row row) {
-        List<String> values= new ArrayList<>();
+        List<String> values = new ArrayList<>();
         Iterator<Cell> cellIterator = row.cellIterator();
-        while(cellIterator.hasNext()) {
+        while (cellIterator.hasNext()) {
             Cell cell = cellIterator.next();
-            switch(cell.getCellType()) {
-                case Cell.CELL_TYPE_BOOLEAN:
-                    values.add(Boolean.valueOf(cell.getBooleanCellValue()).toString());
-                    break;
-                case Cell.CELL_TYPE_NUMERIC:
-                    values.add(Double.valueOf(cell.getNumericCellValue()).toString());
-                    break;
-                case Cell.CELL_TYPE_STRING:
-                    values.add(cell.getStringCellValue());
-                    break;
-            }
+            values.add(getValueOrEmpty(cell));
         }
         return values;
     }
 
-    private String getValueOrEmpty(List<String> fields, int index) {
-        if (fields.isEmpty() || fields.size() <= index) {
+    private String getValueOrEmpty(Cell cell) {
+        if (cell == null) {
             return "";
         }
-        return fields.get(index);
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_BOOLEAN:
+                return Boolean.valueOf(cell.getBooleanCellValue()).toString();
+            case Cell.CELL_TYPE_NUMERIC:
+                return Double.valueOf(cell.getNumericCellValue()).toString();
+            case Cell.CELL_TYPE_STRING:
+                return cell.getStringCellValue();
+            case Cell.CELL_TYPE_BLANK:
+                return "";
+            case Cell.CELL_TYPE_ERROR:
+                return "ERRO";
+            case Cell.CELL_TYPE_FORMULA:
+                return cell.getCellFormula();
+            default:
+                return "";
+        }
     }
 
     public Workbook getWorkbook() throws IOException, InvalidFormatException {
-        if(excel != null) {
+        if (excel != null) {
             return WorkbookFactory.create(excel);
         }
         return WorkbookFactory.create(excelInputStream);
