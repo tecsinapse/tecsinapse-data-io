9b06b3457863c18431555ab9c240fb136c5f33d0
Bruno César <bruno@brunocesar.com>
2016-06-26 22:09:34 -0300

    Creating settings.xml and change travis config

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-if [[ "${TRAVIS_JDK_VERSION}" != "oraclejdk7" ]]; then
+if [[ $TRAVIS_JDK_VERSION != "oraclejdk7" ]]; then
     echo "Skipping after_success actions for JDK version \"${TRAVIS_JDK_VERSION}\""
-    exit
+    exit $?
 fi
 
 mvn -B jacoco:report coveralls:report
 
-if [[ -n ${TRAVIS_TAG} ]]; then
+if [[ -n $TRAVIS_TAG ]]; then
     echo "Skipping deployment for tag \"${TRAVIS_TAG}\""
-    exit
+    exit $?
 fi
 
-if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+if [[ $TRAVIS_BRANCH != "master" ]]; then
     echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
-    exit
+    exit $?
 fi
 
-if [[ "${TRAVIS_PULL_REQUEST}" = "true" ]]; then
-    echo "Skipping deployment for pull request"
-    exit
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+    mvn -B deploy -Dmaven.test.skip=true -Dfindbugs.skip=true -DperformRelease=true --settings $GPG_DIR/settings.xml
+    exit $?
 fi
-
-mkdir deploy
-
-echo "<settings><servers><server><id>sonatype-nexus</id><username>\${env.SONATYPE_USERNAME}</username><password>\${env.SONATYPE_PASSWORD}</password></server></servers></settings>" > $GPG_DIR/settings.xml
-
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
-
-mvn -B deploy -Dmaven.test.skip=true -Dfindbugs.skip=true -DperformRelease=true --settings $GPG_DIR/settings.xml
